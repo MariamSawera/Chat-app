e81ccac4537688f39833abb1c876eb64aac1c40d
MariamSawera <mariamsawera@gmail.com>
2026-01-16 18:12:07 +0500

    added all auth routes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
@@ -100,8 +101,32 @@ res.status(200).json({message: "Logged out successfully"});
 export const updateProfile = async(req,res) => {
     try{
 
-    }catch {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({message: "Profile pic is required"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadResponse.secure_url}, {new:true})
+        res.status(200).json(updatedUser);
+
+    }catch(error){
+        console.log("Error in update profile", error.message);
+        res.status(500).json({message: "Internal Server Error"});
         
     }
 }
 
+
+
+export const checkAuth = (req,res) => {
+    try{
+        res.status(200).json(req.user);
+
+    }catch(error){
+        console.log("Error in checkAuth cocntroller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
