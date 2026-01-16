06d83a919bb5fe60fb9e5acef1497fca94231ac3
MariamSawera <mariamsawera@gmail.com>
2026-01-16 14:35:03 +0500

    add route + controller setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js"
 
  const app = express();
+ app.use(express.json());
+ app.use("/api/auth", authRoutes)
+
 
  app.listen(5001, () => {
     console.log("Server is running on Port 5001");
