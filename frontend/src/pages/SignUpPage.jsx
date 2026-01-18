d795b7d880c061507a778464628fd1e95973a170
MariamSawera <mariamsawera@gmail.com>
2026-01-19 01:22:19 +0500

    added signup UI

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
-import { useAuthStore } from '../store/useAuthStore';
-import { Mail, MessageSquare, User, Lock, Eye, EyeOff } from 'lucide-react';
+import { useAuthStore } from '../store/useAuthStore.js';
+import { Mail, MessageSquare, User, Lock, Eye, EyeOff, Loader2 } from 'lucide-react';
+import { Link } from 'react-router-dom';
+import AuthImagePattern from '../components/AuthImagePattern.jsx';
+import toast from 'react-hot-toast';
 
 const SignUpPage = () => {
 
@@ -11,14 +14,28 @@ const SignUpPage = () => {
     password: "",
   });
 
-  const { signup, isSignUp} = useAuthStore();
+  const { signup,  isSigningUp} = useAuthStore();
 
   const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
+    if (!formData.password) return toast.error("Password is required");
+    if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
+
+    return true;
+
 
   }
 
     const handleSubmit = (e) => {
       e.preventDefault();
+
+    const success = validateForm();
+
+    if (success === true) signup(formData);
+  
+
     
   }
 
@@ -84,7 +101,7 @@ const SignUpPage = () => {
                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                   <Lock className="size-5 text-base-content/40"/>
                 </div>
-                <input type={showPassword ? "text" : "password"} className={`input input-bordered w-full pl-10`} placeholder='.......' value={formData.password} onChange={(e)=>setFormData({...formData, password: e.target.value})} />
+                <input type={showPassword ? "text" : "password"} className={`input input-bordered w-full pl-10`} placeholder='●●●●●●●' value={formData.password} onChange={(e)=>setFormData({...formData, password: e.target.value})} />
                 <button type="button" className="absolute inset-y-0 right-0 pr-3 flex items-center"
                 onClick={() => setShowPassword(!showPassword)}>
                   {showPassword? (
@@ -95,13 +112,31 @@ const SignUpPage = () => {
 
                 </button>
               </div>
-
             </div>
+            <button type="submit" className="btn btn-primary w-full" disabled={isSigningUp}>
+              {
+                isSigningUp ? (
+                  <><Loader2 className="size-5 animate-spin"/> Loading...</>
+                ) : (
+                  "Create Account"
+                )
+              }
+
+            </button>
           </form>
-
+          <div className="text-center">
+            <p className="text-base-content/60">
+            Already have an account?  {""}
+            <Link to="/login" className="link link-primary">Sign in</Link></p>
+          </div>
         </div>
-
       </div>
+
+      {/* right side */}
+      <AuthImagePattern 
+      title= "join our community"
+      subtitle="Connect with friends, share moments, and stay in touch with your loved ones. "/>
+
     </div>
   )
 }
