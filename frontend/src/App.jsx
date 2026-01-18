5da9d9b8f23d518a0a1d18a3028a68295865cc1b
MariamSawera <mariamsawera@gmail.com>
2026-01-18 18:26:16 +0500

    added tailwind + daisyui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function App() {
+  return (
+    // <h1 className="text-3xl font-bold underline">
+<button className="btn  bg-orange-600
+">Default</button>    
+  )
+}
