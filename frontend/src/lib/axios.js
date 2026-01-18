8ff8386b267c33b0bb891cda54a1e990721c54a2
MariamSawera <mariamsawera@gmail.com>
2026-01-18 20:38:11 +0500

    pages

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+
+// instance to use throughout app
+
+import axios from "axios"
+
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:5001/api",
+    withCredentials: true,    //send cookie in every req
+})
